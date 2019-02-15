d7d9103d09a417de0ceb658cec0b1fa351d52702
tsubasa1122 <nakanot@open8.com>
2019-02-15 15:12:17 +0900

    initial commit

---
+++ src/components/List.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export class List extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const list = this.props.todos.map(todo => {
+      return (
+        <li>
+          #{todo.id} {todo.title}
+          <button
+            onClick={() => {
+              this.props.deleteTodo(todo.id);
+            }}
+          >
+            delete
+          </button>
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <h2>List</h2>
+        <ul>{list}</ul>
+      </div>
+    );
+  }
+}
